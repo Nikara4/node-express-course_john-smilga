ef30d9a70b339e1d0341cd819414abf6fd1181a2
Nikara4'git config --global user.email olaszczur93@gmail.comgit config --global user.name "Nikara4 <olaszczur93@gmail.com'git config --global user.user Nikara4git config --global user.email "olaszczur93@gmail.com>
2022-02-07 15:37:01 +0100

    node tutorials covering up to async patterns

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-// npm - global command, comes with node
-// npm --version
+const { readFile, writeFile } = require("fs");
+const util = require("util");
 
-// local dependency - use it only in this particular project
-// npm i / install <packageName>
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
-// global dependency - use it in any project
-// npm install -g <packageName>
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/first.txt", "utf-8");
+    const second = await readFilePromise("./content/second.txt", "utf-8");
+    await writeFilePromise(
+      "./content/res-mind-grenade.txt",
+      `THIS IS AWESOME!!!, ${first} and ${second}`,
+      "utf-8"
+    );
+    console.log(first, second);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf-8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
