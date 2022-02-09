b9d9e1746ce46a2d9adbe76a7830c5e2ec6a7eb4
Nikara4'git config --global user.email olaszczur93@gmail.comgit config --global user.name "Nikara4 <olaszczur93@gmail.com'git config --global user.user Nikara4git config --global user.email "olaszczur93@gmail.com>
2022-02-09 16:19:02 +0100

    tutorial completed until POST method

--- 02-express-tutorial/final/10-middleware-options.js
+++ 02-express-tutorial/final/10-middleware-options.js
@@ -1,28 +1,30 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
-//  req => middleware => res
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const logger = require("./logger");
+const authorise = require("./authorise");
 
-// app.use([logger, authorize])
-// app.use(express.static('./public'))
-app.use(morgan('tiny'))
+// req => middleware => res
+// app.use([logger, authorise]);
 
-app.get('/', (req, res) => {
-  res.send('Home')
-})
-app.get('/about', (req, res) => {
-  res.send('About')
-})
-app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
-app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('Items')
-})
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log("server is listening on port 5000...");
+});
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const { people } = require("./02-express-tutorial/data");
+
+// status assets
+app.use(express.static("./02-express-tutorial/methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/people", (req, res) => {
+  //   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please provide credentials");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000...");
+});
